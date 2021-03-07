4b0bec8c03e15bfcf55c4e8192dcd554e5789eaf
fox <1025687605@qq.com>
2021-03-07 11:24:33 +0800

    feat: add cli

--- packages/core/src/node/cli.ts
+++ packages/core/src/node/cli.ts
@@ -1,29 +1,30 @@
 import minimist from 'minimist'
 import chalk from 'chalk'
-import { execSync } from 'child_process'
+import { createServer } from 'vite'
 
 const argv = minimist(process.argv.slice(2))
 
 console.log(chalk.cyan(`best v${require('../package.json').version}`))
 console.log(chalk.cyan(`vite v${require('vite/package.json').version}`))
 
-const exec = (command: string) => {
-  execSync(command)
-}
-
 const run = async () => {
   const command = argv._[0]
 
-  if (command === 'server') {
-    exec('vite server')
+  // start command
+  if (!command || command === 'dev') {
     return
   }
 
+  // build command
   if (command === 'ssr') {
+    return
   }
 
   if (command === 'ssg') {
+    return
   }
+
+  console.error(`invalid command.`)
 }
 
 run().catch(error => {
